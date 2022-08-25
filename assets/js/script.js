6fb0119173399e2608c2672af252fc7dccf20e2c
Aleph Null <likhonhasankomool@gmail.com>
2022-08-25 18:36:28 -0400

    QOL changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,10 +11,18 @@ function taskFormHandler(event)
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+    if(!taskNameInput||!taskTypeInput)
+    {
+        alert("Please fill out the form");
+        return false;
+    }
+
     var taskDataObj = {
         name:taskNameInput,
         type:taskTypeInput
     };
+
+    formEl.reset();
 
     createTaskEl(taskDataObj);
 }
