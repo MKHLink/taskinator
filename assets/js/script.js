9543070f1f422861483b634218815681be5a0660
Aleph Null <likhonhasankomool@gmail.com>
2022-08-25 18:21:40 -0400

    added ability to add task name and display type

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 var formEl = document.querySelector("#task-form");
 
-formEl.addEventListener("click",createTaskHandler);
+formEl.addEventListener("submit",createTaskHandler);
 
-function createTaskHandler()
+
+function createTaskHandler(event)
 {
     event.preventDefault();
 
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
     var listItemEl = document.createElement("li");
     listItemEl.className="task-item";
-    listItemEl.textContent="New Task";
+
+    var taskInfoEl=document.createElement("div");
+    taskInfoEl.className="task-info";
+
+    taskInfoEl.innerHTML="<h3 class='task-name'>"+taskNameInput+"</h3><span class='task-type'>"+ taskTypeInput+"</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+    
     tasksToDoEl.appendChild(listItemEl);
+
 }
+
